b52373fbcc5d913d61572a03a795586f3ef43638
ChitvanGarg2000 <chitvangarg0305@gmail.com>
2025-11-15 16:43:46 +0530

    added cloudinary variable

--- client/src/components/Shared/UploadAvatar.tsx
+++ client/src/components/Shared/UploadAvatar.tsx
@@ -18,7 +18,7 @@ const UploadAvatar: React.FC<UploadAvatarProps> = ({ onFileUpload }) => {
       formData.append("upload_preset", "connectMe");
       const resp = await fetch(
         `https://api.cloudinary.com/v1_1/${
-          import.meta.env.VITE_CLOUD_NAME
+          import.meta.env.CLOUDINARY_CLOUD_NAME
         }/image/upload`,
         {
           method: "POST",
